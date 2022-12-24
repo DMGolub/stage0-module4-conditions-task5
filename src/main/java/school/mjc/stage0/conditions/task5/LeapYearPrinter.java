080b71156856b170cceb07e7e9d330eb94197f89
Dmitry Golub <dmgolub@mail.ru>
2022-12-24 22:41:21 +0400

    Task completed.

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
+
     public void isLeapYear(int year) {
+        if (isLeap(year)) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
+    }
+
+    private boolean isLeap(int year) {
+        return year % 100 == 0 && year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
     }
 }
